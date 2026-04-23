8008cbeb833188435188fa0ca0aed4016bc567e5
jrcmafc <jesus.rene.correa.martinez@gmail.com>
2026-04-22 18:51:18 -0700

    Implement post fetching and rendering on DOMContentLoaded
    Add functionality to fetch and display posts from a JSON file, sorted by date.

--- script.js
+++ script.js
@@ -3,3 +3,29 @@ const btn = document.getElementById("toggleDark");
 btn.onclick = () => {
   document.body.classList.toggle("dark");
 };
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("posts.json")
+    .then(res => res.json())
+    .then(posts => {
+
+      // Ordenar por fecha (más nuevos primero)
+      posts.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+      const container = document.getElementById("postsContainer");
+
+      posts.forEach(post => {
+        container.innerHTML += `
+          <article class="card">
+            <img src="${post.image}" alt="post">
+            <div class="card-content">
+              <h3><a href="${post.link}">${post.title}</a></h3>
+              <p class="meta">${new Date(post.date).toLocaleDateString()}</p>
+              <p class="tags">${post.tags}</p>
+              <p>${post.excerpt}</p>
+            </div>
+          </article>
+        `;
+      });
+    });
+});
